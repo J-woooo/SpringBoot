b770e296784d6870c579d01c8d6e419a002f35e0
Bae Jin Woo <jw969696@naver.com>
2021-02-10 23:55:23 +0900

    의존관계주입

--- 김영한-스프링핵심/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 김영한-스프링핵심/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,14 +3,15 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderServiceImpl implements OrderService{
 
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
